c7a1e72170757d5a621ca0fd398f7bb966aa62d4
Danila Palcech <dpalcech@gmail.com>
2022-12-22 18:03:48 +0200

    google

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import Logo from "./components/Logo";
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";  
+import { About } from "./components/About";
+import { Product } from "./components/Product";
+import { Community } from "./components/Community";
 
 function App() {
   return (
     <div className="App">
-      <Logo/>
+      <header>
+        <Link to="/Product">Product</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Community">Community</Link>
+      </header>
+
+
+      <Routes>
+        <Route path="/Product" element={<Product/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Community" element={<Community/>}/>
+      </Routes>
+
     </div>
   );
 }
